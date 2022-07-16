a5198f8999200b26f48b7acaab6466b5e8117737
AnikKDev <anikdev2016@gmail.com>
2022-07-16 15:38:06 +0600

    created all the necessary routes with react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from '../src/Pages/Shared-Folder/Header';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Products from './Pages/Products/Products';
+import About from './Pages/About/About';
+import Blogs from './Pages/Blogs/Blogs';
+import Contact from './Pages/Contact/Contact';
 function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/home" element={<Home></Home>} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
+
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BannerCarousel from '../../Utilities/Carousel/BannerCarousel';
+
+const Home = () => {
+    return (
+        <div>
+            <BannerCarousel />
+        </div>
+    );
+};
+
+export default Home;
