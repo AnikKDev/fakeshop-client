9b7640e82de2b821f47849d708870cada5654fc7
AnikKDev <anikdev2016@gmail.com>
2022-07-16 16:23:25 +0600

    completed commitment

---
+++ src/Pages/Home/Commitment/Commitment.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Commitment.css';
+const Commitment = () => {
+    return (
+        <div className="my-5 d-flex justify-content-center align-items-center">
+            <div className="text-center w-50">
+                <h2 className="my-5">WE ARE COMMITTED</h2>
+                <p className="text-muted fst-italic">
+                    We are commited to providing best service possible to our customers. We are gaining more and more appreciations day by day from various kinds of customers <b>across the globe</b>. And yes, you can order from anywhere around the world.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Commitment;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import BannerCarousel from '../../Utilities/Carousel/BannerCarousel';
+import Commitment from './Commitment/Commitment';
 
 const Home = () => {
     return (
         <div>
             <BannerCarousel />
+            <Commitment />
         </div>
     );
 };
